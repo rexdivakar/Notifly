6a0f8e6887dd18913691481d3f13a410bdd04cc8
rex_divakar <rexdivakar@hotmail.com>
2020-10-24 16:45:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", encoding="utf-8") as f:
  
 setup(
   name='notifly',
-  version='1.1.3',
+  version='1.1.4',
   description='Telegram Bot Notifier',
   long_description=read_file,
   long_description_content_type='text/markdown',
