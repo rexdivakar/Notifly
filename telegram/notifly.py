aa7fb497fbe2345f5e004cbadb017ec4668bc1d3
Kirk <matthewkk7@gmail.com>
2020-10-25 17:26:03 -0400

    Commiting working version of the progress bar after some testing.

--- telegram/notifly.py
+++ telegram/notifly.py
@@ -2,9 +2,10 @@
 import requests
 import datetime
 import sys
+from tqdm import tqdm
+import time
 
-token = ''  # Token of your bot
-
+token = '1347043184:AAGmj_-VlRsIyQGSTTTcSFMhBJh3-aPQkto'  # Token of your bot
 
 class BotHandler:
     def __init__(self, token):
@@ -12,19 +13,41 @@ class BotHandler:
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
     #url = "https://api.telegram.org/bot<token>/"
-    
+
+    # Progress bar method to call,
+    # response is the http post sent
+    # data_type is the type of data being send
+    def create_progress_bar(self, response, data_type):
+        total = int(response.headers.get('content-length'))
+
+        if data_type == 'img':
+            progress_bar = tqdm(total=total, unit=' bits', desc='Uploading img')
+            for data in response.iter_content(512):
+                progress_bar.update(len(data))
+            progress_bar.close()
+        else:
+            progress_bar = tqdm(total=total, unit=' bits', desc='Uploading Doc')
+            for data in response.iter_content(512):
+                progress_bar.update(len(data))
+            progress_bar.close()
+
 
     def get_updates(self, offset=0, timeout=30):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
+        print(resp)
         try:
+            print(f'about to return {resp}')
+            print(f'result data is {resp.json()["result"]}')
             return resp.json()['result']
         except:
+            print('about to fail')
             return '404 error'
 
     def send_message(self, msg,notification):
         fetch_updates = self.get_updates()
+        print(fetch_updates)
         chat_id = fetch_updates[0]['message']['chat']['id']
         # update_id = fetch_updates[0]['update_id']
         
@@ -36,9 +59,12 @@ class BotHandler:
         fetch_updates = self.get_updates()
         chat_id = fetch_updates[0]['message']['chat']['id']
         method = 'sendPhoto?'+'chat_id='+str(chat_id)
-        
+
         files ={'photo':open(img_path, 'rb')}
-        resp = requests.post(self.api_url+method,files=files)
+
+        resp = requests.post(self.api_url+method,files=files,stream=True)
+        self.create_progress_bar(resp, 'img')
+        print("Complete.")
 
 
     def send_document(self,file_path):
@@ -47,10 +73,7 @@ class BotHandler:
         method = 'sendDocument?'+'chat_id='+str(chat_id)
         
         files ={'document':open(file_path, 'rb')}
-        resp = requests.post(self.api_url+method,files=files)
-        
+        resp = requests.post(self.api_url+method,files=files,stream=True)
 
-    
-
-
-
+        self.create_progress_bar(resp,'doc')
+        print("Complete.")
