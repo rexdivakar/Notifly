6ba917c1d538c544c6a6e9f3dffc80c23689bc37
Kirk <matthewkk7@gmail.com>
2020-10-25 23:06:19 -0400

    rebasing the notify.py file from the main branch https://github.com/rexdivakar/Notifly.git to resolve a conflict.

--- telegram/notifly.py
+++ telegram/notifly.py
@@ -1,36 +1,20 @@
 # -*- coding: UTF8 -*-
+import json
+import os
+
 import requests
-import datetime
-import sys
-from tqdm import tqdm
-import time
 
 token = ''  # Token of your bot
 
+
 class BotHandler:
-    def __init__(self, token):
+    def __init__(self, token, dirdownloads='./downloads'):
         self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.file_url = "https://api.telegram.org/file/bot{}/".format(token)
+        self.dirDownloads = dirdownloads
 
-    #url = "https://api.telegram.org/bot<token>/"
-
-    # Progress bar method to call,
-    # response is the http post sent
-    # data_type is the type of data being send
-    def create_progress_bar(self, response, data_type):
-        total = int(response.headers.get('content-length'))
-
-        if data_type == 'img':
-            progress_bar = tqdm(total=total, unit=' bits', desc='Uploading img')
-            for data in response.iter_content(512):
-                progress_bar.update(len(data))
-            progress_bar.close()
-        else:
-            progress_bar = tqdm(total=total, unit=' bits', desc='Uploading Doc')
-            for data in response.iter_content(512):
-                progress_bar.update(len(data))
-            progress_bar.close()
-
+    # url = "https://api.telegram.org/bot<token>/"
 
     def get_updates(self, offset=0, timeout=30):
         method = 'getUpdates'
@@ -41,34 +25,97 @@ class BotHandler:
         except:
             return '404 error'
 
-    def send_message(self, msg,notification):
+    def send_message(self, msg, notification):
         fetch_updates = self.get_updates()
         chat_id = fetch_updates[0]['message']['chat']['id']
         # update_id = fetch_updates[0]['update_id']
-        
-        params = {'chat_id': chat_id, 'text': msg, 'parse_mode': 'HTML','disable_notification':notification}
+
+        params = {'chat_id': chat_id, 'text': msg, 'parse_mode': 'HTML', 'disable_notification': notification}
         method = 'sendMessage'
         return requests.post(self.api_url + method, params)
 
-    def send_image(self,img_path):
+    def send_image(self, img_path):
         fetch_updates = self.get_updates()
         chat_id = fetch_updates[0]['message']['chat']['id']
-        method = 'sendPhoto?'+'chat_id='+str(chat_id)
+        method = 'sendPhoto?' + 'chat_id=' + str(chat_id)
 
-        files ={'photo':open(img_path, 'rb')}
+        files = {'photo': open(img_path, 'rb')}
+        return requests.post(self.api_url + method, files=files)
 
-        resp = requests.post(self.api_url+method,files=files,stream=True)
-        self.create_progress_bar(resp, 'img')
-        print("Complete.")
-
-
-    def send_document(self,file_path):
+    def send_document(self, file_path):
         fetch_updates = self.get_updates()
         chat_id = fetch_updates[0]['message']['chat']['id']
-        method = 'sendDocument?'+'chat_id='+str(chat_id)
-        
-        files ={'document':open(file_path, 'rb')}
-        resp = requests.post(self.api_url+method,files=files,stream=True)
+        method = 'sendDocument?' + 'chat_id=' + str(chat_id)
 
-        self.create_progress_bar(resp,'doc')
-        print("Complete.")
+        files = {'document': open(file_path, 'rb')}
+        return requests.post(self.api_url + method, files=files)
+
+    def download_files(self):
+        fetch_updates = self.get_updates()
+        for update in fetch_updates:
+            fileName = ''
+            media = ['document', 'photo', 'video', 'voice']
+            # Check if update message contains any of the media keys from above
+            intersection = list(set(update['message'].keys()) & set(media))
+            if intersection:
+                mediaKey = intersection[0]
+
+                # Determine fileId. For photos multiple versions exist. Use the last one.
+                if mediaKey == 'photo':
+                    fileId = update['message'][mediaKey][-1]['file_id']
+                else:
+                    fileId = update['message'][mediaKey]['file_id']
+
+                if mediaKey == 'document':
+                    # In a document, it's possible to use the original name.
+                    fileName = update['message'][mediaKey]['file_name']
+                self.get_file(fileId, fileName)
+
+    def get_file(self, file_id, filename=''):
+        """"Follow the getFile endpoint and download the file by file_id.
+        A fileName can be given, else a file_unique_id gibberish name will be used.
+
+        See also: https://core.telegram.org/bots/api#getfile
+        """
+        method = 'getFile?' + 'file_id=' + str(file_id)
+        res = requests.post(self.api_url + method, file_id)
+        try:
+            filePath = res.json()['result']['file_path']
+            # Determine the fileName. Use modified filePath if none given.
+            if not filename:
+                filename = filePath[filePath.rfind('/') + 1:]
+        except (KeyError, ValueError):
+            return "500 - Failed parsing the file link from API response."
+
+        if not os.path.exists(self.dirDownloads):
+            os.mkdir(self.dirDownloads)
+
+        localPath = os.path.join(self.dirDownloads, filename)
+
+        # Download file as stream.
+        res = requests.get(self.file_url + filePath, stream=True)
+        if res.status_code == 200:
+            try:
+                with open(localPath, 'wb') as f:
+                    for chunk in res:
+                        f.write(chunk)
+            except IOError:
+                pass
+            return '200 - {} written.'.format(localPath)
+        else:
+            return '404 - Error accessing {}'.format(filePath)
+
+    def session_dump(self):
+        '''
+        Session Download manager
+        '''
+        resp = self.get_updates()
+        try:
+            if not os.path.exists(self.dirDownloads):
+                os.mkdir(self.dirDownloads)
+            localPath = os.path.join(self.dirDownloads, 'session_dump.json')
+
+            with open(localPath, 'w+', encoding='utf-8') as outfile:
+                json.dump(resp, outfile)
+        except:
+            return 'Dump Error'
