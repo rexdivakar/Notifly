1c36e174e5dbd3a74f0d2c19b789e049a15dc516
sanchitjain123 <sanchitjain1996@gmail.com>
2020-10-29 23:48:13 +0530

    successfully map parameters to parameter values, get model instance and its history

--- temp.py
+++ temp.py
@@ -1,15 +1,39 @@
 from notifly import discord
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-
-token = 'https://discord.com/api/webhooks/770164527911534634/BUthlz8AwweN6otan37h3lpZM9kcXgBdO77-M5Zc9GHz9kacL1sCYVK_pLyVBkMq1zkP'
+token='https://discord.com/api/webhooks/771253998313144321/5FNBsAi8-exyc-3rxLOYudQdQqMdpcaCOqCh6z1IX4ledG1oGyaONeIY1HDUns6qBZhW'
 x = discord.Notifier(token)
+
+import inspect
 
 def test(f):
     def inner(*args, **kwargs):
-        args[0].stop_training
-        x.send(f"{args[1]}. {args[2]}")
-        f(*args, **kwargs)
+        # combine parameters from the arguments into a single list
+        parameter_values = []
+        for i in args:
+            parameter_values.append(i)
+
+        for i in kwargs.values():
+            parameter_values.append(i)
+        print(parameter_values)
+        print(parameter_values)
+        sig = inspect.signature(f)
+        parameters = sig.parameters.keys()
+        print(parameters)
+        d = dict(zip(parameters, parameter_values))
+        print(d)
+        # TODO: in default parameters we wont get the default value if the value is not passed during the function call
+
+        model_instance = d.get('self').model
+        print('\n')
+        hist = model_instance.history.history
+        print('\n')
+        epoch = d.get('epoch')
+        logs = d.get('logs')
+
+        x.send(f"{epoch}. {hist}", print_message=False)
+        r = f(*args, **kwargs)
+        return r
     return inner
 
 
@@ -21,13 +45,6 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-# train_images = train_images / 255.0
-
-# test_images = test_images / 255.0
-
-
-
 
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -41,10 +58,8 @@ model.compile(optimizer='adam',
 
 
 class testCallback(tf.keras.callbacks.Callback):
-
-    @test(interval=1, )
+    @test
     def on_epoch_end(self, epoch, logs=None):
-
         print(tf.keras.optimizers)
 
 model.fit(train_images, train_labels, epochs=5,callbacks=[testCallback()])
