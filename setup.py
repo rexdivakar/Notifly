7d4f317548e6c1fa14db1c636c328aac02224dc9
rexdivakar <rexdivakar@hotmail.com>
2022-05-23 08:35:48 +0530

    1.3.2 (23/5/2022) ------------------ - Resolved Buffer Overflow in NumPy Package (CVE-2021-33430) - Updated Wiki pages

--- setup.py
+++ setup.py
@@ -15,16 +15,16 @@ with open("README.md", encoding="utf-8") as f:
  
 setup(
   name='notifly',
-  version='1.3.1',
+  version='1.3.2',
   description='Notification on the fly !',
   long_description=read_file,
   long_description_content_type='text/markdown',
-  url='https://github.com/rexdivakar/Telegram-Notifly',  
-  author='Divakar R, Sanchit Jain' ,
+  url='https://github.com/rexdivakar/Telegram-Notifly',
+  author='Divakar R, Sanchit Jain',
   author_email='rexdivakar@hotmail.com , sanchitjain1996@gmail.com',
-  license='MIT', 
+  license='MIT',
   classifiers=classifiers,
-  keywords='Bot Notification', 
+  keywords='Bot Notification',
   packages=['notifly'],
   install_requires=['requests', 'numpy==1.19.3', 'matplotlib', 'slackclient', 'python-dotenv', 'psutil']
 )
